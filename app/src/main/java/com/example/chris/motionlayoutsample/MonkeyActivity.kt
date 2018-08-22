b107dc067fc5b179f3103125de938e98a352de78
crehmann <crehmann@users.noreply.github.com>
2018-08-22 16:28:10 +0200

    added project

---
+++ app/src/main/java/com/example/chris/motionlayoutsample/MonkeyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.chris.motionlayoutsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MonkeyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_monkey)
+    }
+}
